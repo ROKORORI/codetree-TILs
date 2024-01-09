ff5eb3593ee53ca917e9dce37713cbb42122fe1a
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-09 17:15:36 +0900

    Title: 변수 값 교체하기 6, Time: 4ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240109/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, tmp;
+    a = 2, b = 5;
+    tmp = b;
+    b = a, a = tmp;
+    std::cout << a << std::endl << b;
+    return 0;
+}
