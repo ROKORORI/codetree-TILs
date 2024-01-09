d9211e80842c74e3490de6abec438e401b7ad697
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-09 17:20:12 +0900

    Title: 변수 값 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240109/변수 값 복사하기/copying-variable-values.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    a = 1, b = 2, c = 3;
+    a = b = c;
+    std::cout << a << " " << b << " " << c;
+    return 0;
+}
