f3f9226798620629f6893e5b5bb0ddf04a170425
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-09 15:39:54 +0900

    Title: 정수 선언하고 뺄셈, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240109/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    int b;
+    int d;
+    a = 97;
+    b = 13;
+    d = a - b;
+    cout << a << ' - ' << b << ' = ' << d;
+    return 0;
+}
