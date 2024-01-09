7ab38d44bb2e3056892bfd1b3dbe01a90dbc7514
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-09 15:54:51 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240109/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    char c;
+    string d;
+    a = 1, b = 2, c = 'C', d = "->";
+    cout << a << d << b << d << c;
+    return 0;
+}
