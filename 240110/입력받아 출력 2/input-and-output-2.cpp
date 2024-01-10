0fc77a0129758a160cd6cf9566e55783ef3f711b
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-10 16:41:06 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int f, h;
+    cin >> f;
+    cin.get();
+    cin >> h;
+
+    cout << f << h;
+    
+    return 0;
+}
