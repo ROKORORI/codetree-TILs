3395de778d79b65d4b6799c978cfcd01d8a6219c
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-11 23:52:16 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    string out;
+
+    cin >> a;
+    out = (a == 1) ? "t" : "f";
+
+    cout << out;
+    return 0;
+}
