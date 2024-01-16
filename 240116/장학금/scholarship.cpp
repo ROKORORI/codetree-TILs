8e36f28f4421589bfaf6ce0b3c2be0706cdf2265
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-16 22:30:10 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240116/장학금/scholarship.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m_s, l_s;
+    cin >> m_s >> l_s;
+
+    if (m_s >= 90) {
+
+        if (l_s >= 95) {
+            cout << 10;
+        }
+        else if (l_s >= 90) {
+            cout << 5;
+        }
+        else {
+            cout << 0;
+        }
+
+    }
+    else {
+        cout << 0;
+    }
+    
+    return 0;
+}
