3efaa624a9bff1bd7e8b60e86f44365c50dc5d83
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-16 23:06:50 +0900

    Title: 최댓값 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240116/최댓값 구하기/maximum-value.cpp
+++ 240116/최댓값 구하기/maximum-value.cpp
@@ -17,15 +17,25 @@ int main() {
     }
     // 적어도 하나는 같다.
     else {
-        if (a == b) {
-            if (a == c) {
-                cout << a;
-            }
-            else if (a > c) {
-                cout << a;
+        if ((a == b) && (a == c)) {
+            cout << a;
+        }
+        else {
+            if (a == b) {
+                if (a > c) {
+                    cout << a;
+                }
+                else {
+                    cout << c;
+                }
             }
             else {
-                cout << c;
+                if (a > b) {
+                    cout << a;
+                }
+                else {
+                    cout << b;
+                }
             }
         }
     }
