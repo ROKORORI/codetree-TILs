35fa19f357c6fc306fa8029077b89e4977badf67
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-18 23:35:27 +0900

    Title: 자연수면 출력, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

--- 240118/자연수면 출력/output-only-natural-number.cpp
+++ 240118/자연수면 출력/output-only-natural-number.cpp
@@ -6,10 +6,13 @@ int main() {
     int a, b;
     cin >> a >> b;
 
-    if (a >= 0) {
+    if (a > 0) {
         for (int i = 0; i < b; i++) {
             cout << a;
         }
+    }
+    else {
+        cout << 0;
     }
     return 0;
 }
