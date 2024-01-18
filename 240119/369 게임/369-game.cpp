edff328ea7b1dc3bc15988158b726ed24d3367c4
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-19 00:02:13 +0900

    Title: 369 게임, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240119/369 게임/369-game.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0) {
+            cout << 0 << " ";
+        }
+        else {
+            int check = 1;
+            string j = to_string(i);
+            for (int k = 0; k <= j.length(); k++) {
+                string l = to_string(j[k]);
+                if (l == "3") {
+                    check = 0;
+                }
+                else if (l == "6") {
+                    check = 0;
+                }
+                else if (l == "9") {
+                    check = 0;
+                }
+            }
+
+            if (check == 0) {
+                cout << 0 << " ";
+            }
+            else {
+                cout << i << " ";
+            }   
+        }
+    }
+    return 0;
+}
