37637fa68dd514869045c5a8c0522c31f5fc661b
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-22 12:59:04 +0900

    Title: 완전수 판별기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240122/완전수 판별기/perfect-number-discriminator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, sum = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        if (n % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum == n) {
+        cout << "P";
+    }
+    else {
+        cout << "N";
+    }
+    
+    return 0;
+}
