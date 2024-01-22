86810aa88bcc682b9340dc1ed877b1e04ae0bab6
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-22 12:26:23 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240122/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int cnt = 0;
+
+    for (int i = 0; i < 5; i++) {
+        int a;
+        cin >> a;
+
+        if (a % 2 == 0) {
+            cnt += 1;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
