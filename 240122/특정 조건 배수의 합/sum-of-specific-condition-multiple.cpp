50227b68308aa0605fd73f0f1ba2c71e6e9f0d65
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-22 12:50:56 +0900

    Title: 특정 조건 배수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240122/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
+++ 240122/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a, b, sum = 0;
+    int a, b, tmp, sum = 0;
     cin >> a >> b;
+
+    if (a >= b) {
+        tmp = b;
+        b = a;
+        a = tmp;
+    }
     
     while (a <= b) {
         if (a % 5 == 0) {
