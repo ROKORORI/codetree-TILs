bdd3efa05956c1439b766bf583e82907b905dd37
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-24 09:53:24 +0900

    Title: 별표 출력하기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/별표 출력하기 5/print-star-5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for (int i = n; i > 0; i--) {
+        for (int j = i; j > 0; j--) {
+            for (int k = i; k > 0; k--){
+                cout << "*";
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
