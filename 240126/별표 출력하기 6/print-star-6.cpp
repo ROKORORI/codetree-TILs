ec85ae07d1b7058ba2892e094a5f4f5d6006ed28
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-26 03:19:01 +0900

    Title: 별표 출력하기 6, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240126/별표 출력하기 6/print-star-6.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < 2 * n - 1; i++) {
+        if (i <= 2) {
+            for (int j = 0; j < 2 * n - 1; j++) {
+                if ((j < i) || (j >= 2 * n - (i + 1))) {
+                    cout << " " << " ";
+                }
+                else {
+                    cout << "*" << " ";
+                }
+            }
+        }
+        else {
+            for (int j = 0; j < 2 * n - 1; j++) {
+                if ((j < 2 * n - (2 + i)) || (j > i)) {
+                    cout << " " << " ";
+                }
+                else {
+                    cout << "*" << " ";
+                }
+            }
+
+        }
+        cout << endl;
+    }
+    return 0;
+}
