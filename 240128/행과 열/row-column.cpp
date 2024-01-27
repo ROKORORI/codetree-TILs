3250e37ee12f6d7e5eda75483c97d4badd50a1e7
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-28 04:46:59 +0900

    Title: 행과 열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240128/행과 열/row-column.cpp
+++ 240128/행과 열/row-column.cpp
@@ -6,7 +6,7 @@ int main() {
     int a, b;
     cin >> a >> b;
 
-    for (int i = 1; i < a; i++) {
+    for (int i = 1; i <= a; i++) {
         for (int j = 1; j <= b; j++) {
             cout << i * j << " ";
         }
