27d5309a63abc3e5ece6a8d5f520592b0521d58d
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-01-30 09:27:25 +0900

    Title: 구구단 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240130/구구단 만들기/print-multiplication-table.cpp
+++ 240130/구구단 만들기/print-multiplication-table.cpp
@@ -7,9 +7,9 @@ int main() {
     cin >> a >> b;
     
     for (int i = 1; i <= 9; i++) {
-        for (int j = b; j > 0; j -= 2) {
+        for (int j = b; j >= a; j -= 2) {
             cout << j << " * " << i << " = " << j * i;
-            if (j != 2) {
+            if (j != a) {
                 cout << " / ";
             }
         }
