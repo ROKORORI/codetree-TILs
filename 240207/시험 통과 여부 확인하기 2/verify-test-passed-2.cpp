9c4cc9050e934405c172c9779ba12a9ad0b586c2
ROKORORI <113521103+ROKORORI@users.noreply.github.com>
2024-02-07 00:20:26 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240207/시험 통과 여부 확인하기 2/verify-test-passed-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, cnt = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int arr[4], sum = 0;
+        for (int j = 0; j < 4; j++) {
+            cin >> arr[j];
+            sum += arr[j];
+        }
+        if (sum/4 >= 60) {
+            cout << "pass" << endl;
+            cnt += 1;
+        }
+        else {
+            cout << "fail" << endl;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
